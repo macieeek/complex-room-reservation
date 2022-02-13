4fe89ed8b853d2788646bac00930ae070c87f867
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-02-13 12:02:20 +0100

    Add sql file with necessary tables

---
+++ complex_room_reservation.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 13 Lut 2022, 12:01
+-- Wersja serwera: 10.4.22-MariaDB
+-- Wersja PHP: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `complex_room_reservation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(100) NOT NULL,
+  `date_res_since` date NOT NULL,
+  `date_res_untill` date NOT NULL,
+  `room_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone_number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(100) NOT NULL,
+  `how_many_people` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `how_many_people`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 1),
+(5, 2),
+(6, 2),
+(7, 3),
+(8, 2),
+(9, 4),
+(10, 2),
+(11, 3),
+(12, 4),
+(13, 1),
+(14, 2),
+(15, 3),
+(16, 4);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
